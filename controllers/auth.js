9e9ffd57d8ce95155d3cd568bafcff7a1e291e3a
Giggs <gigasii2856@gmail.com>
2021-08-19 14:53:02 +0800

    Updated sendgrid api key

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,199 +1,189 @@
 // Third-party packages
-const bcrypt = require('bcryptjs');
-const nodemailer = require('nodemailer');
-const sendgridTransport = require('nodemailer-sendgrid-transport');
-const {validationResult} = require('express-validator');
+const bcrypt = require("bcryptjs");
+const nodemailer = require("nodemailer");
+const sendgridTransport = require("nodemailer-sendgrid-transport");
+const { validationResult } = require("express-validator");
 
 // Core packages
-const crypto = require('crypto');
+const crypto = require("crypto");
 
 // Imports
 const User = require("../models/user");
 
 // Constants
-const SENDER_EMAIL = 'gigasii2856@gmail.com';
+const SENDER_EMAIL = "pokemon2856@gmail.com";
 
 // Intialization
-const transporter = nodemailer.createTransport(sendgridTransport({
-    auth: {api_key: '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'}
-}));
+const transporter = nodemailer.createTransport(
+  sendgridTransport({
+    auth: { api_key: `${process.env.SENDGRID_API_KEY}` },
+  })
+);
 
-function getFlash(req, field)
-{
-    // Once a flash variable is retrived, it is removed from the session
-    let message = req.flash(field);
-    
-    if (message.length > 0)
-    {
-        return message[0];
-    }
+function getFlash(req, field) {
+  // Once a flash variable is retrived, it is removed from the session
+  let message = req.flash(field);
 
-    return null;
+  if (message.length > 0) {
+    return message[0];
+  }
+
+  return null;
 }
 
 exports.getLogIn = (req, res, next) => {
-    res.render('auth/login', {
-        path: '/login',
-        pageTitle: 'Login',
-        errorMessage: getFlash(req, 'error')
-    });
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    errorMessage: getFlash(req, "error"),
+  });
 };
 
 exports.postLogIn = (req, res, next) => {
-    User.findOne({email: req.body.email})
-    .then(user => {
-        // Email doesnt match
-        if (!user)
-        {
-            req.flash('error', 'Invalid email');
-            return res.redirect('/login');
-        }
-        // Email matches, continue to comparing password
-        bcrypt.compare(req.body.password, user.password)
-        .then(match => {
-            // Password matches
-            if (match)
-            {
-                req.session.isLoggedIn = true;
-                req.session.user = user;
-                return req.session.save(() => {
-                    res.redirect('/')
-                });
-            }
-            // Password doesnt match
-            req.flash('error', 'Invalid password');
-            res.redirect('/login');
+  User.findOne({ email: req.body.email }).then((user) => {
+    // Email doesnt match
+    if (!user) {
+      req.flash("error", "Invalid email");
+      return res.redirect("/login");
+    }
+    // Email matches, continue to comparing password
+    bcrypt.compare(req.body.password, user.password).then((match) => {
+      // Password matches
+      if (match) {
+        req.session.isLoggedIn = true;
+        req.session.user = user;
+        return req.session.save(() => {
+          res.redirect("/");
         });
+      }
+      // Password doesnt match
+      req.flash("error", "Invalid password");
+      res.redirect("/login");
     });
+  });
 };
 
 exports.postLogOut = (req, res, next) => {
-    // Destroy all session variables
-    req.session.destroy(() => res.redirect('/'));
+  // Destroy all session variables
+  req.session.destroy(() => res.redirect("/"));
 };
 
-exports.getSignUp = (req, res, next) => { 
-    res.render('auth/signup', {
-        path: '/signup',
-        pageTitle: 'Signup',
-        errorMessage: getFlash(req, 'error'),
-    });
+exports.getSignUp = (req, res, next) => {
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
+    errorMessage: getFlash(req, "error"),
+  });
 };
 
 exports.postSignUp = (req, res, next) => {
-    // Build-in validation
-    const errors = validationResult(req);
-    if (!errors.isEmpty())
-    {
-        return res.status(422).render('auth/signup', {
-            path: '/signup',
-            pageTitle: 'Signup',
-            errorMessage: errors.array()[0].msg
-        });
+  // Build-in validation
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    return res.status(422).render("auth/signup", {
+      path: "/signup",
+      pageTitle: "Signup",
+      errorMessage: errors.array()[0].msg,
+    });
+  }
+
+  // Custom validation, if successful, create user
+  User.findOne({ email: req.body.email }).then((user) => {
+    if (user) {
+      req.flash("error", "Email already exists");
+      return res.redirect("/signup");
     }
 
-    // Custom validation, if successful, create user
-    User.findOne({email: req.body.email})
-    .then(user => {
-        if (user)
-        {
-            req.flash('error', 'Email already exists');
-            return res.redirect('/signup');
-        }
+    if (req.body.password != req.body.confirmPassword) {
+      req.flash("error", "Passwords do not match");
+      return res.redirect("/signup");
+    }
 
-        if (req.body.password != req.body.confirmPassword)
-        {
-            req.flash('error', 'Passwords do not match');
-            return res.redirect('/signup');
-        }
-
-        // Hash password
-        bcrypt.hash(req.body.password, 12)
-        .then(hashedPw => {
-            // New user
-            const newUser = new User({
-                email: req.body.email,
-                password: hashedPw,
-                cart: {products: []}
-            });
-            return newUser.save();
-        })
-        .then(() => {
-            // Send confirmation email
-            transporter.sendMail({
-                to: req.body.email,
-                from: SENDER_EMAIL,
-                subject: 'Sign up confirmation',
-                html: '<h1>You have successfully signed up.</h1>'
-            });
-            res.redirect('/login');
+    // Hash password
+    bcrypt
+      .hash(req.body.password, 12)
+      .then((hashedPw) => {
+        // New user
+        const newUser = new User({
+          email: req.body.email,
+          password: hashedPw,
+          cart: { products: [] },
         });
-    });
-}
+        return newUser.save();
+      })
+      .then(() => {
+        // Send confirmation email
+        transporter.sendMail({
+          to: req.body.email,
+          from: SENDER_EMAIL,
+          subject: "Sign up confirmation",
+          html: "<h1>You have successfully signed up.</h1>",
+        });
+        res.redirect("/login");
+      });
+  });
+};
 
 exports.getReset = (req, res, next) => {
-    res.render('auth/reset', {
-        path: '/reseet',
-        pageTitle: 'Reset',
-        errorMessage: getFlash(req, 'error')
-    });
-}
+  res.render("auth/reset", {
+    path: "/reseet",
+    pageTitle: "Reset",
+    errorMessage: getFlash(req, "error"),
+  });
+};
 
 exports.postReset = (req, res, next) => {
-    crypto.randomBytes(32, (err, buffer) => {
-        const token = buffer.toString('hex');
-        User.findOne({email: req.body.email})
-        .then(user => {
-            if (!user)
-            {
-                req.flash('error', 'No account with that email found');
-                return res.redirect('/reset');
-            }
-            user.resetToken = token;
-            // Specify in milliseconds
-            user.resetTokenExpiration = Date.now() + 3600000;
-            user.save()
-            .then(() => {
-                transporter.sendMail({
-                    to: req.body.email,
-                    from: SENDER_EMAIL,
-                    subject: 'Password reset',
-                    html: `
+  crypto.randomBytes(32, (err, buffer) => {
+    const token = buffer.toString("hex");
+    User.findOne({ email: req.body.email }).then((user) => {
+      if (!user) {
+        req.flash("error", "No account with that email found");
+        return res.redirect("/reset");
+      }
+      user.resetToken = token;
+      // Specify in milliseconds
+      user.resetTokenExpiration = Date.now() + 3600000;
+      user.save().then(() => {
+        transporter.sendMail({
+          to: req.body.email,
+          from: SENDER_EMAIL,
+          subject: "Password reset",
+          html: `
                     <p>You requested a password reset</p>
                     <a href="http://localhost:3000/reset/${token}">Click here to change password</a>
-                    `
-                });
-                res.redirect('/products');
-            });
+                    `,
         });
+        res.redirect("/products");
+      });
     });
-}
+  });
+};
 
 exports.getNewPassword = (req, res, next) => {
-    User.findOne({
-        resetToken: req.params.token, 
-        resetTokenExpiration: {$gt: Date.now()}
-    })
-    .then(user => {
-        res.render('auth/new-password', {
-            path: '/newpassword',
-            pageTitle: 'New password',
-            errorMessage: getFlash(req, 'error'),
-            user: user
-        });
+  User.findOne({
+    resetToken: req.params.token,
+    resetTokenExpiration: { $gt: Date.now() },
+  }).then((user) => {
+    res.render("auth/new-password", {
+      path: "/newpassword",
+      pageTitle: "New password",
+      errorMessage: getFlash(req, "error"),
+      user: user,
     });
-}
+  });
+};
 
 exports.postNewPassword = (req, res, next) => {
-    User.findOne({resetToken: req.body.passwordToken})
-    .then(user => {
-        bcrypt.hash(req.body.password, 12)
-        .then(hashedPw => {
-            user.password = hashedPw;
-            user.resetToken = undefined;
-            user.resetTokenExpiration = undefined;
-            return user.save()
-        })
-        .then(() => res.redirect('/login'));
-    });
-}
+  User.findOne({ resetToken: req.body.passwordToken }).then((user) => {
+    bcrypt
+      .hash(req.body.password, 12)
+      .then((hashedPw) => {
+        user.password = hashedPw;
+        user.resetToken = undefined;
+        user.resetTokenExpiration = undefined;
+        return user.save();
+      })
+      .then(() => res.redirect("/login"));
+  });
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,34 +1,34 @@
 // Third-party packages
-const express = require('express');
-const {body} = require('express-validator');
+const express = require("express");
+const { body } = require("express-validator");
 
 // Imports
-const authController = require('../controllers/auth');
+const authController = require("../controllers/auth");
 
 // Initilization
 const router = express.Router();
 
 // Set routes
-router.get('/login', authController.getLogIn);
-router.post('/login', authController.postLogIn);
-router.post('/logout', authController.postLogOut);
-router.get('/signup', authController.getSignUp);
-router.get('/reset', authController.getReset);
-router.post('/reset', authController.postReset);
-router.get('/reset/:token', authController.getNewPassword); 
-router.post('/new-password', authController.postNewPassword);
+router.get("/login", authController.getLogIn);
+router.post("/login", authController.postLogIn);
+router.post("/logout", authController.postLogOut);
+router.get("/signup", authController.getSignUp);
+router.get("/reset", authController.getReset);
+router.post("/reset", authController.postReset);
+router.get("/reset/:token", authController.getNewPassword);
+router.post("/new-password", authController.postNewPassword);
 
-router.post('/signup',
-[
-    body('email', 'Invalid email')
-        .isEmail()
-        .normalizeEmail(),
-    body('password', 'Invalid password')
-        .isLength({min: 5, max: 20})
-        .isAlphanumeric()
-        .trim()
-],
-authController.postSignUp);
+router.post(
+  "/signup",
+  [
+    body("email", "Invalid email").isEmail().normalizeEmail(),
+    body("password", "Invalid password")
+      .isLength({ min: 1, max: 20 })
+      .isAlphanumeric()
+      .trim(),
+  ],
+  authController.postSignUp
+);
 
 // Export
 module.exports = router;
